b439ed4508b3f7918ce8c0630c28208b2696b060
oshritush <oshrite2@gmail.com>
2016-05-16 18:13:42 +0300

    GREEN:correct code for test 2 check_if_person_in_location

--- src/map.js
+++ src/map.js
@@ -1,3 +1,4 @@
+		var locations = ["Israel","Langtang","Langtang valley"];
 
 function Map(posts) {
   this._posts = posts;
@@ -15,7 +16,21 @@ return array
   };
 
 Map.prototype.check_if_person_in_location = function(name) {
-return null;
+
+		var flag = false;
+		this._posts.forEach(function (item) {
+		if( item.indexOf(name)>-1)
+			{
+			locations.forEach(function (loc) 
+				{	
+			if( item.indexOf(loc)>-1 && item.indexOf(" at ")>-1)
+					{
+						flag=true;
+					}
+				});
+			}
+		});
+return flag;	
 }
 
 module.exports = Map;
