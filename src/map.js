80bf9e4896dcdeb74c2b4d9a49ccb48b8b9a7ed5
ChagitY <chagit1234@gmail.com>
2016-05-23 17:13:20 +0300

    GREEN: check test with person with same name and different location

--- src/map.js
+++ src/map.js
@@ -1,5 +1,5 @@
-var locations = ["Israel","Langtang","Langtang valley"];
-var names = ["Israel","Langtang","Langtang valley"];
+var locations = ["Israel","Langtang","Bangkok"];
+var names = ["Or A.","Or B.","Or C."];
 
 function Map(posts) {
   this._posts = posts;
@@ -35,9 +35,43 @@ return flag;
 }
 
 
-Map.prototype.check_persons_with_the_same_name_with_different_locations = function(name) {
-	return null;
+Map.prototype.check_persons_with_the_same_name_with_different_locations = function() {
+	
+	var res = false;
+	
+	
+	var cmap = new Map(this._posts);
+	//go over all the possible names
+	names.forEach(function(name) {
+		var array = [];	
+		console.log(name);
+		//for each name find all its locations
+		var person_locations = cmap.find_a_person(name);
+		console.log(person_locations);
+		//add the locations for specific name to an array
+		person_locations.forEach(function(value) {
+
+			locations.forEach(function(location) {
+				if (value.indexOf(location) > -1)
+{
+console.log(location);
+					array.push(location);	
 }
+			});
+		});
+console.log(array);
+		//check if in the array of locations of specific name there is duplication 
+		if (array.length > 1) {
+			for (var i = 0; i < array.length; i++) {
+				for (var j = 0; j < array.length; j++) {
+					if (i != j && array[i] != array[j])
+						res = true;
+				}
+			}
+		}	
+	});
+	return res;
+};
 
 
 module.exports = Map;
